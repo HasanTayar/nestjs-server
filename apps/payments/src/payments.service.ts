a3a464bba56187e063e232c83c58747767f85a7d
QPM <admin@QuantumPinnacleMatrix.tech>
2024-03-05 14:55:48 +0200

    added an emit bettwen notifaction server and payments server and updated the payments

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,9 @@
-import { CreateChargeDto } from '@app/common';
-import { Injectable } from '@nestjs/common';
+import { NOTIFICATIONS_SERVICE } from '@app/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ClientProxy } from '@nestjs/microservices';
 import { Stripe } from 'stripe';
+import { PaymentsCreateChargeDto } from './dto/payments-create-charge.dto';
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(
@@ -10,8 +12,12 @@ export class PaymentsService {
       apiVersion: '2023-10-16',
     },
   );
-  constructor(private readonly configService: ConfigService) {}
-  async createCharge({ card, amount }: CreateChargeDto) {
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
+  async createCharge({ card, amount, email }: PaymentsCreateChargeDto) {
     const paymentMethod = await this.stripe.paymentMethods.create({
       type: 'card',
       card,
@@ -22,6 +28,9 @@ export class PaymentsService {
       confirm: true,
       payment_method_types: ['card'],
       currency: 'usd',
+    });
+    this.notificationsService.emit('notify_email', {
+      email,
     });
     return paymentIntent;
   }
