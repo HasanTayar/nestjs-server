7f254e6d537b7be48b44339cee869c271b40620b
QPM <admin@QuantumPinnacleMatrix.tech>
2024-03-09 01:26:17 +0200

    fixing the return statment on jwt

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -4,10 +4,20 @@ describe('Reservations', () => {
       email: 'test@test.com',
       password: 'StrongPassword123!',
     };
-    await fetch('http://auth:3001', {
+    await fetch('http://auth:3001/users', {
       method: 'POST',
       body: JSON.stringify(user),
+      headers: { 'Content-Type': 'application/json' },
     });
+    const res = await fetch('http://auth:3001/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const jwt = await res.text();
+    console.log(jwt);
   });
-  test('Create', () => {});
+  test('Create', () => {
+    expect(true).toBeTruthy();
+  });
 });
