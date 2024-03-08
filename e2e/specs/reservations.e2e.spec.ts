73e5edbc722f71b31ba7e8d5e2b6035e3dd054d6
QPM <admin@QuantumPinnacleMatrix.tech>
2024-03-09 01:50:37 +0200

    done with the test e2e

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -1,23 +1,69 @@
 describe('Reservations', () => {
+  let jwt: string;
+
   beforeAll(async () => {
     const user = {
       email: 'test@test.com',
-      password: 'StrongPassword123!',
+      password: 'StrogPassword123!@',
     };
     await fetch('http://auth:3001/users', {
       method: 'POST',
       body: JSON.stringify(user),
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
-    const res = await fetch('http://auth:3001/auth/login', {
+    const response = await fetch('http://auth:3001/auth/login', {
       method: 'POST',
       body: JSON.stringify(user),
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
-    const jwt = await res.text();
-    console.log(jwt);
+    jwt = await response.text();
   });
-  test('Create', () => {
-    expect(true).toBeTruthy();
+
+  test('Create & Get', async () => {
+    const createdReservation = await createReservation();
+    const responseGet = await fetch(
+      `http://reservations:3000/reservations/${createdReservation._id}`,
+      {
+        headers: {
+          Authentication: jwt,
+        },
+      },
+    );
+    const reservation = await responseGet.json();
+    expect(createdReservation).toEqual(reservation);
   });
+
+  const createReservation = async () => {
+    const responseCreate = await fetch(
+      'http://reservations:3000/reservations',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authentication: jwt,
+        },
+        body: JSON.stringify({
+          startDate: '02-01-2023',
+          endDate: '02-05-2023',
+          placeId: '123',
+          invoiceId: '123',
+          charge: {
+            amount: 13,
+            card: {
+              cvc: '413',
+              exp_month: 12,
+              exp_year: 2027,
+              number: '4242 4242 4242 4242',
+            },
+          },
+        }),
+      },
+    );
+    expect(responseCreate.ok).toBeTruthy();
+    return responseCreate.json();
+  };
 });
