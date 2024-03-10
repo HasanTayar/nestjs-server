f75cd7daf5350c474479e9c192926a045cdda101
QPM <admin@QuantumPinnacleMatrix.tech>
2024-03-11 00:52:45 +0200

    changing from TCP to RabbitMQ

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -32,10 +32,10 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       {
         name: AUTH_SERVICE,
         useFactory: (configService: ConfigService) => ({
-          transport: Transport.TCP,
+          transport: Transport.RMQ,
           options: {
-            host: configService.get('AUTH_HOST'),
-            port: configService.get('AUTH_PORT'),
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'auth',
           },
         }),
         inject: [ConfigService],
@@ -43,10 +43,10 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       {
         name: PAYMENTS_SERVICE,
         useFactory: (configService: ConfigService) => ({
-          transport: Transport.TCP,
+          transport: Transport.RMQ,
           options: {
-            host: configService.get('PAYMENTS_HOST'),
-            port: configService.get('PAYMENTS_PORT'),
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'payments',
           },
         }),
         inject: [ConfigService],
